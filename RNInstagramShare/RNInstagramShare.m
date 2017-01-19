0fe6ae07858c998605e5d47741fe00838c21795f
Andrew Tsvirko <Andrew Tsvirko>
2017-01-19 16:30:52 +0300

    corrected method name

--- RNInstagramShare/RNInstagramShare.m
+++ RNInstagramShare/RNInstagramShare.m
@@ -12,7 +12,7 @@
 RCT_EXPORT_MODULE()
 
 
-RCT_EXPORT_METHOD() {
+RCT_EXPORT_METHOD(share:) {
     
     NSURL *instagramURL       = [NSURL URLWithString:[NSString stringWithFormat:@"instagram://camera"]];
     
